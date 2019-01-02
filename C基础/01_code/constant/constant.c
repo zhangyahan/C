25bbc42e5e29acae5d95872db4f83b418ace2f47
zyh <zyahan1997@163.com>
2019-01-02 19:25:50 +0800

    c

--- C基础/01_code/constant/constant.c
+++ C基础/01_code/constant/constant.c
@@ -1,12 +1,16 @@
+
+
+
+
 #include <stdio.h>
 
-/*
- * constant demo
- */
- 
- int main()
- {
-    printf("character: %s\ninteger: %d\nfloating point: %f\n", "Zhang sir", 21, 1.70);
-    printf("%%\n");
+int main()
+{
+    printf("你好\n");
     return 0;
- }
+
+}
+
+
+
+
